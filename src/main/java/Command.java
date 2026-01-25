0e3d5721628a5d8d74508872d88220735d8dc1eb
BaoLam <tranbaolam006@gmail.com>
2026-01-25 19:15:09 +0800

    develop: mark and unmark command

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -3,23 +3,25 @@ package main.java;
 public class Command {
     private boolean isDone;
     private String name;
-
+    // Constructor
     public Command( String name) {
         this.isDone = false;
         this.name = name;
     }
-
-    public void setdone() {
-        isDone = true;
+    // Getter and Setter
+    public String getName() {
+        return name;
     }
-
+    public void setDone() {
+        this.isDone = true;
+    }
     public void setUndone() {
-        isDone = false;
+        this.isDone = false;
     }
-
+    //Convert into format : [ ] command
     public String toString(){
         String res ="";
-        res = (this.isDone) ? "[X]" : "[]" + " " + name;
+        res = ((this.isDone) ? "[X]" : "[ ]") + " " + name;
         return res;
     }
 }
--- src/main/java/Nuke.java
+++ src/main/java/Nuke.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Nuke {
     private static Command[] commands = new Command[100];
     private static int numOfCommand = 0;
-    private static void greet(){
-        System.out.println("\t===============================");
-        System.out.println("\tHi! Nuke's waiting for your command!");
-        System.out.println("\tWho do you want me to nuke today?");
-        separate();
-    }
-
-    private static void exit(){
-        separate();
-        System.out.println("\tKaboommm!");
-        System.out.println("\tI have destroyed our current session!");
-        System.out.println("\tSee you later!");
-        System.out.println("\t===============================");
+    private static void announce(String mode){
+        if(mode.equals("greet")){
+            System.out.println("\t===============================");
+            System.out.println("\tHi! Nuke's waiting for your command!");
+            System.out.println("\tWho do you want me to nuke today?");
+            separate();
+        }
+        else{// default to exit
+            separate();
+            System.out.println("\tKaboommm!");
+            System.out.println("\tI have destroyed our current session!");
+            System.out.println("\tSee you later!");
+            System.out.println("\t===============================");
+        }
     }
 
     private static void separate(){
@@ -25,8 +26,9 @@ public class Nuke {
     }
 
     private static boolean receiveCommand(String command){
+
         if(command.equals("bye")){ // Handle bye directly
-            exit();
+            announce("exit");
             return false;
         }
         separate();
@@ -35,19 +37,38 @@ public class Nuke {
         return true;
     }
 
-    private static void handleCommand(String command) {
-        if(command.equals("list")){
+    private static void handleCommand(String commandLine) {
+        String[] parsedComment = commandLine.split(" ");
+        if(parsedComment[0].equals("list")){// list command
             for(int i = 0; i < numOfCommand;i++){
-                System.out.printf("%d.%s%n", i, commands[i].toString());
+                System.out.printf("%d.%s%n", i+1, commands[i].toString());
             }
             return;
         }
-        commands[numOfCommand++] = new Command(command);
-        System.out.printf("\tAdded: %s%n", command);
+        if(parsedComment[0].equals("mark") || parsedComment[0].equals("unmark")){ //mark and unmark command
+            try{
+                int chosenCommand = Integer.parseInt(parsedComment[1]);
+                if(parsedComment[0].equals("mark")){
+                    commands[chosenCommand -1].setDone();
+                    System.out.printf("\tMark the command: %s%n", commands[chosenCommand - 1].getName());
+                }
+                else{
+                    commands[chosenCommand -1].setUndone();
+                    System.out.printf("\tUnmark the command: %s%n", commands[chosenCommand - 1].getName());
+                }
+
+            }
+            catch (Exception e){
+                System.out.println("\tYour command is wrong, please try again!");
+            }
+            return;
+        }
+        commands[numOfCommand++] = new Command(commandLine);
+        System.out.printf("\tReceive command: %s%n", commandLine);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        greet();
+        announce("greet");
         // Hand over execution for handle()
         while(receiveCommand(in.nextLine())){
         }
