2d95e5155e414475ad6a1344cb90214584b83024
BaoLam <tranbaolam006@gmail.com>
2026-01-25 18:23:30 +0800

    develop: bye command and some simple text add

--- src/main/java/Nuke.java
+++ src/main/java/Nuke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Nuke {
+    private static void greet(){
+        System.out.println("\t===============================");
+        System.out.println("\tHi! Nuke's waiting for your command!");
+        System.out.println("\tWho do you want me to nuke today?");
+        separate();
+    }
+
+    private static void exit(){
+        System.out.println("\tKaboommm!");
+        System.out.println("\tI have destroyed our current session!");
+        System.out.println("\tSee you later!");
+        System.out.println("\t===============================");
+    }
+
+    private static void separate(){
+        System.out.println("\t-------------------------------");
+    }
+
+    private static boolean handle(String line){
+        if(line.equals("bye")){
+            separate();
+            exit();
+            return false;
+        }
+        separate();
+        System.out.printf("\t%s%n",line);
+        separate();
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("============================");
-        System.out.println("Hello! I'm Nuke");
-        System.out.println("What can I do for you?");
-        System.out.println("----------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("============================");
+        Scanner in = new Scanner(System.in);
+        greet();
+        // Hand over execution for handle()
+        while(handle(in.nextLine())){
+        }
     }
 }
